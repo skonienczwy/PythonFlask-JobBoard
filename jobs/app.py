32aeaab30a3bab0a4ae61d4dac4547064500d6a5
skonienczwy <skonienczwy@hotmail.com>
2019-06-13 21:26:48 +0200

    SQL Implemented

--- jobs/app.py
+++ jobs/app.py
@@ -10,9 +10,9 @@ def open_connection():
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
-    return  connection
+    return connection
 
-def execute_sql(sql,values=(),commit=False,single=False):
+def execute_sql(sql, values=(), commit=False,single=False):
     connection = open_connection()
     cursor = connection.execute(sql,values)
     if commit == True:
@@ -33,4 +33,7 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql('SELECT job.id, job.title, job.description,'
+                       'job.salary, employer.id as employer_id, employer.name as employer_name'
+                       'FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html',jobs=jobs)
